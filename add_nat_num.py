965ce098e497e69ed425022ed03554bb4abaad6d
Michael Caron <michael.r.caron@gmail.com>
2012-01-10 13:59:18 -0600

    faster implementation, more LOC

--- add_nat_num.py
+++ add_nat_num.py
@@ -1,10 +1,28 @@
-def alg(n):
+def alg(max):
     res = 0
-    for x in range(n):
-        if (x % 5 == 0 or x % 3 == 0):
-            res += x
-    return res
+    iterations = 0
+    fivemaxhit = False
+    threemaxhit = False
+
+    
+    for x in range(max):
+        if fivemaxhit and threemaxhit: break
+
+        if not fivemaxhit:
+            fiver = x * 5
+            if fiver < max: 
+                res += fiver
+            else:
+                fivemaxhit = True
+        if not threemaxhit:
+            threer = x * 3
+            if threer < max: 
+                res += threer
+            else:
+                threemaxhit = True
+        iterations += 1
+    return (res, iterations)
 
 if __name__ == "__main__":
-    print "Result (small set): %s" % alg(10)
-    print "Result (big set): %s" % alg(1000)
+    print "Result (small set): %s, iterations: %d" % alg(10)
+    print "Result (big set): %s, iterations: %d" % alg(1000)
