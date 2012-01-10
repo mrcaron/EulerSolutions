2cd1461935fd285005bb221bf404f1b03899d397
Michael Caron <michael.r.caron@gmail.com>
2012-01-10 14:36:16 -0600

    Added test runner, encoded algorithms as methods

--- add_nat_num.py
+++ add_nat_num.py
@@ -1,10 +1,8 @@
-def alg(max):
+def short(max):
     res = 0
     iterations = 0
     fivemaxhit = False
     threemaxhit = False
-
-    
     for x in range(max):
         if fivemaxhit and threemaxhit: break
 
@@ -23,6 +21,24 @@ def alg(max):
         iterations += 1
     return (res, iterations)
 
+def obvious(max):
+    res = 0
+    iterations = 0
+    for x in range(max):
+        if (x % 5 == 0 or x % 3 == 0): res += x
+        iterations += 1
+    return (res, iterations)
+
+def runtimed(alg, *set):
+    from time import clock, time
+    algname = alg.__name__
+    for n in set:
+        start = clock()
+        (res,it) = alg(n)
+        elapsed = (clock() - start)
+        print "Result (%s(%d)): %s, iterations: %d, in %.5f seconds" % (algname, n, res, it, elapsed)
+
 if __name__ == "__main__":
-    print "Result (small set): %s, iterations: %d" % alg(10)
-    print "Result (big set): %s, iterations: %d" % alg(1000)
+    runtimed(short, 10, 1000)
+    runtimed(obvious, 10, 1000)
+    
