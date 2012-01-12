cd8d244fda2ca5860008739ce014f19c54e3226f
Michael Caron <michael.r.caron@gmail.com>
2012-01-12 09:57:58 -0600

    added other implementations to see how to get fastest computation

--- add_nat_num.py
+++ add_nat_num.py
@@ -1,6 +1,21 @@
+def setunion(max):
+    return sum(list(set([i for i in range(3,max,3)]).union(set([j for j in range(5,max,5)]))))
+
 def reduced(max):
-    res = reduce( lambda x, y: x + ( (y % 5 == 0 or y % 3 == 0) and y or 0 ), range(0,max))
-    return res
+    val = reduce(lambda x,y: x+y, filter(lambda x: x % 3 == 0 or x % 5 == 0, range(max)))
+    return val
+
+def comprehend(max):
+    return sum([x for x in range(max) if x%3 == 0 or x%5 == 0])
+
+def rang(max):
+    val = sum(range(0,max,3)) + sum(range(0,max,5)) - sum(range(0,1000,15))
+
+# implementation of summation formula (currently fastest impl!
+def formulaHelper(max, multiple):
+    return (((int((max-1)/multiple)) * ((int((max-1)/multiple))+1)) / 2) * multiple
+def formula(max):
+    return formulaHelper(max, 3) + formulaHelper(max, 5) - formulaHelper(max, 15)
 
 def short(max):
     res = 0
@@ -27,6 +42,8 @@ def short(max):
                 threemaxhit = True
     return res
 
+# this can be written concisely as:
+#    reduce(add,[x for x in range(max) if x % 3 == 0 or x % 5 == 0])
 def obvious(max):
     res = 0
     for x in range(max):
@@ -40,10 +57,14 @@ def runtimed(alg, *set):
         start = clock()
         res = alg(n)
         elapsed = (clock() - start)
-        print "Result (%s(%d)): %s, in %.5f seconds" % (algname, n, res, elapsed)
+        print "Result (%s(%d)): %s, in %.7f seconds" % (algname, n, res, elapsed)
 
 if __name__ == "__main__":
-    runtimed(short, 10, 1000)
-    runtimed(obvious, 10, 1000)
-    runtimed(reduced, 10, 1000)
+    runtimed(short, 1000)
+    runtimed(obvious, 1000)
+    runtimed(reduced, 1000)
+    runtimed(rang, 1000)
+    runtimed(formula, 1000)
+    runtimed(comprehend, 1000)
+    runtimed(setunion, 1000)
     
