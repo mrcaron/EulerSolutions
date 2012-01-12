2b6b894ca012bbe3dcf9c8e9fda1526530f12264
Michael Caron <michael.r.caron@gmail.com>
2012-01-12 14:49:11 -0600

    added fibonacci solution

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+import math
+import functools
+import operator 
+
+def sumFibo(n):
+    return sum(i for i in fibo2(upto=n) if not i%2)
+
+def fibo2(a=-1,b=1,upto=4000000):
+    while a+b < upto:
+        a,b = b,a+b
+        yield b
+
+if __name__ == "__main__":
+    print "Sum of 4000000 fibonacci numbers: %d" % sumFibo(4000000)
